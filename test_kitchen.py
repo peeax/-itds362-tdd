2dd78f93caf8154eab2b59c88d2291827a9b2491
pongsatach boonkaew <pongsatach9@gmail.com>
2026-09-03 12:32:50 +0700

    A5 REFACTOR: add grams and ounces factory functions

--- test_kitchen.py
+++ test_kitchen.py
@@ -11,22 +11,28 @@ from kitchen import Quantity
 
 
 def test_multiplication():
-    flour = Quantity(200, "g")
+    flour = grams(200)
     assert flour.times(3).amount == 600
 
 def test_multiplication_by_two():
-    flour = Quantity(200, "g")
+    flour = grams(200)
     assert flour.times(2).amount == 400
 
 def test_multiplication_returns_a_new_quantity():
-    flour = Quantity(200, "g")
+    flour = grams(200)
 
     assert flour.times(3).amount == 600
     assert flour.times(2).amount == 400
 
 def test_equality():
-    assert Quantity(200, "g") == Quantity(200, "g")
-    assert Quantity(200, "g") != Quantity(300, "g")
+    assert grams(200) == grams(200)
+    assert grams(200) != grams(300)
 
 def test_grams_are_not_ounces():
-    assert Quantity(1, "g") != Quantity(1, "oz")
+    assert grams(1) != ounces(1)
+
+def grams(amount):
+    return Quantity(amount, "g")
+
+def ounces(amount):
+    return Quantity(amount, "oz")
