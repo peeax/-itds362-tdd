1251ba2dab259728c747b82e281f89711c9b5699
pongsatach boonkaew <pongsatach9@gmail.com>
2026-09-03 12:37:50 +0700

    A6 RED: return Sum from plus

--- kitchen.py
+++ kitchen.py
@@ -7,7 +7,7 @@ class Quantity:
         return Quantity(self.amount * multiplier, self.unit)
 
     def plus(self, other):
-        return Quantity(500, "g")
+        return Sum(self, other)
 
     def __eq__(self, other):
         return (
@@ -21,3 +21,8 @@ class Quantity:
 class Converter:
     def reduce(self, expression, unit):
         return expression
+
+class Sum:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
